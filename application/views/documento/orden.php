593134864cda77daab4cac67e3f510394cd0db43
Rildo Gomez <rildo.gomez@osctelecoms.com>
2018-12-08 15:13:21 -0500

    Se modifica modulo orden de compra

--- application/views/documento/orden.php
+++ application/views/documento/orden.php
@@ -74,7 +74,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Detalle de la orden '{{orden}}'</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Detalle de la orden <strong>'{{orden}}'</strong></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
